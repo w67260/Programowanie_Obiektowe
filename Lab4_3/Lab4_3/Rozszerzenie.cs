31603ffe1a2d4b1de189e88b3b409bc0326715ab
w67260 <116088861+w67260@users.noreply.github.com>
2024-01-15 17:01:40 +0100

    Lab4

---
+++ Lab4_3/Lab4_3/Rozszerzenie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab4_3
+{
+    internal static class RozszerzeniaOsob
+    {
+        //Metoda wypisujaca osoby z listy
+        public static void WypiszOsoby(this List<IOsoba> listaOsob)
+        {
+            foreach (var osoba in listaOsob)
+            {
+                Console.WriteLine(osoba.ZwrocPelnaNazwe());
+            }
+        }
+
+        //Metoda sotrująca osoby w liscie
+        public static void PosortujOsobyPoNazwisku(this List<IOsoba> listaOsob)
+        {
+            listaOsob.Sort((o1, o2) => o1.Nazwisko.CompareTo(o2.Nazwisko));
+        }
+    }
+}
---
+++ Lab4_3/Lab4_3/StudentWSIiZ.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4_3
+{
+    internal class StudentWSIiZ : Student
+    {
+        public override void WypiszPelnaNazweIUczelnie()
+        {
+            Console.WriteLine($"{base.ZwrocPelnaNazwe()} - WSIiZ");
+        }
+    }
+}
